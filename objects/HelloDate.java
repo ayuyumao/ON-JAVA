f8c673d96f657f1ce4ef048e6ecfa74ee98c67e2
阿鱼与猫 <ayuyumao@163.com>
2022-12-17 23:40:52 +0800

    提交objects
    源代码

---
+++ objects/HelloDate.java
@@ -0,0 +1,11 @@
+// objects/HelloDate.java
+package objects;
+
+import java.util.*;
+
+public class HelloDate{
+    public static void main(String[] args){
+        System.out.println("Hello,it's: ");
+        System.out.println(new Date());
+    }
+}
---
+++ objects/ShowProperties.java
@@ -0,0 +1,11 @@
+// objects/ShowProperties.java
+package objects;
+
+public class ShowProperties {
+    public static void main(String[] args){
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(
+            System.getProperty("java.library.path"));
+    }
+}
